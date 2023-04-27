64a80b94650ad1333068381dbd9c9bcb0cec3229
Nikita7312 <104190128+Nikita7312@users.noreply.github.com>
2023-04-27 15:46:09 +0400

    Add files via upload

---
+++ ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,56 @@
+﻿//Добавить проверку на корректность пользовательского ввода, чтобы избежать исключений при некорректном вводе.
+//Добавить возможность для пользователя повторно вводить число N в случае некорректного ввода.
+//Добавить комментарии к коду, чтобы сделать его более понятным и легким для чтения.
+using System.Text;
+
+namespace Library
+{
+    public class ZadanieOne
+    {
+        public void One()
+        {
+            Console.Write("Введите число N: ");
+            int n;
+            while (!int.TryParse(Console.ReadLine(), out n)) // проверка на корректный ввод числа
+            {
+                Console.Write("Некорректный ввод. Введите число N: ");
+            }
+            string result = string.Join(", ", Enumerable.Range(1, n));
+            Console.WriteLine(result);
+        }
+    }
+    public class ZadanieTwo
+    {
+        public static void Two()
+        {
+            Console.Write("Введите нечетное число N: ");
+            int n;
+            while (!int.TryParse(Console.ReadLine(), out n) || n % 2 == 0) // проверка на корректный ввод нечетного числа
+            {
+                Console.Write("Некорректный ввод. Введите нечетное число N: ");
+            }
+            for (int row = 1; row <= n; row++)
+            {
+                for (int col = 1; col <= n; col++)
+                {
+                    if (row == n / 2 + 1 && col == n / 2 + 1) Console.Write(" ");
+                    else Console.Write("#");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // вызов методов из классов ZadanieOne и ZadanieTwo
+            ZadanieOne zadanieOne = new ZadanieOne();
+            zadanieOne.One();
+
+            ZadanieTwo.Two();
+
+            Console.ReadKey();
+        }
+    }
+}
